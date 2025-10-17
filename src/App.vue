63dbd64ef92595f221b16e8ee49c67c54736edaf
dalentri <aydenjmason05@gmail.com>
2025-10-16 22:49:17 -0500

    Increased app margin

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-col m-50 gap-5">
+  <div class="flex flex-col m-70 gap-5">
     <div class="flex flex-row">
       <h1 class="text-5xl font-bold flex">Splash</h1>
       <div class="text-lg flex items-center justify-end w-full"><h6>5/20</h6></div>
