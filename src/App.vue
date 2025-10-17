cafb3fe909847cd8c600ea4704535b92e3604e24
dalentri <aydenjmason05@gmail.com>
2025-10-16 22:17:59 -0500

    Centered content, added progress-bar, and added card for content

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="flex flex-col m-50 gap-5">
+    <h1 class="text-5xl justify-center flex">Splash</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <progress class="progress" value="" max="100"></progress>
+    <div class="card bg-base-100">
+      <div class="card-body"></div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
