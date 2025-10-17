7b539d895ab4530fc74c43de7f7ab4c4ddbb8f52
dalentri <aydenjmason05@gmail.com>
2025-10-16 22:36:42 -0500

    pushed card number to the right

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
 
 <template>
   <div class="flex flex-col m-50 gap-5">
-    <h1 class="text-5xl justify-center flex">Splash</h1>
+    <div class="flex flex-row">
+      <h1 class="text-5xl font-bold flex">Splash</h1>
+      <div class="text-lg flex items-center justify-end w-full"><h6>5/20</h6></div>
+    </div>
 
-    <progress class="progress" value="" max="100"></progress>
-    <div class="card bg-base-100">
+    <progress class="progress" value="40" max="100"></progress>
+    <div class="card bg-base-300 shadow-lg">
       <div class="card-body"></div>
     </div>
   </div>
