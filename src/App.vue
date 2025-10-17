7463fae246e5dcb1b024db5e3adbeaba9ec94dea
dalentri <aydenjmason05@gmail.com>
2025-10-17 15:43:54 -0500

    Centered content and made it obey a max size

--- src/App.vue
+++ src/App.vue
@@ -21,36 +21,39 @@ const handleView = () => {
 </script>
 
 <template>
-  <div class="flex flex-col m-70 gap-5">
-    <div class="flex flex-row">
-      <h1 class="text-5xl font-bold flex">Splash</h1>
-      <div class="text-lg flex items-center justify-end w-full"><h6>5/20</h6></div>
-    </div>
+  <!-- HACK: Temporarily made the screen sort of centered -->
+  <div class="h-screen flex items-center justify-center">
+    <div class="flex flex-col w-full gap-5 max-w-5xl mx-auto items-center mb-15">
+      <div class="flex flex-row w-full">
+        <h1 class="text-5xl font-bold flex">Splash</h1>
+        <div class="text-lg flex items-center justify-end w-full"><h6>5/20</h6></div>
+      </div>
 
-    <progress class="progress" value="40" max="100"></progress>
-    <div class="card bg-base-300 shadow-lg">
-      <div class="card-body">
-        <div class="h-50 flex">
-          <p v-if="current_side == 'question'" class="text-3xl flex justify-center items-center">
-            Sample Text of the front side of the card
-          </p>
-          <p v-else-if="current_side == 'answer'" class="text-lg flex justify-center items-center">
-            Answer side of the flash card
-          </p>
-        </div>
-        <div class="bg-base-600 flex flex-row w-full">
-          <div class="w-1/3 flex justify-start">
-            <button class="btn font-light">< Previous</button>
+      <progress class="progress w-full" value="40" max="100"></progress>
+
+      <div class="card bg-base-300 shadow-lg w-full">
+        <div class="card-body">
+          <div class="h-50 flex justify-center items-center">
+            <p v-if="current_side == 'question'" class="text-3xl text-center w-full">
+              Sample Text of the front side of the card
+            </p>
+            <p v-else-if="current_side == 'answer'" class="text-lg text-center w-full">
+              Answer side of the flash card
+            </p>
           </div>
 
-          <div class="w-1/3 flex justify-center">
-            <button class="btn" @click="handleView">
-              {{ view_button_text }}
-            </button>
-          </div>
-
-          <div class="w-1/3 flex justify-end">
-            <button class="btn justify-end">Next ></button>
+          <div class="bg-base-600 flex flex-row w-full">
+            <div class="w-1/3 flex justify-start">
+              <button class="btn font-light">< Previous</button>
+            </div>
+            <div class="w-1/3 flex justify-center">
+              <button class="btn" @click="handleView">
+                {{ view_button_text }}
+              </button>
+            </div>
+            <div class="w-1/3 flex justify-end">
+              <button class="btn">Next ></button>
+            </div>
           </div>
         </div>
       </div>
