51c092b5a54c7cda0c5d71d44f8195248972c855
dalentri <aydenjmason05@gmail.com>
2025-10-18 17:58:08 -0500

    Added card index handling based on json

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import sample_cards from './data/samplecards.json'
 
-// TODO: Make a it so where the card can flip
 const question_side = ref('')
 const answer_side = ref('')
 const view_button_text = ref('Show Answer')
-
-//TODO: Make handling of which side the card is on
 const current_side = ref('question')
+
+const current_index = ref(0)
+const cards = ref(sample_cards)
+
+const prevCard = () => {
+  if (current_index.value > 0) {
+    current_index.value--
+  }
+}
+
+const nextCard = () => {
+  if (current_index.value < cards.value.length - 1) {
+    current_index.value++
+  }
+}
 
 const handleView = () => {
   if (current_side.value == 'question') {
@@ -21,30 +34,34 @@ const handleView = () => {
 </script>
 
 <template>
-  <!-- HACK: Temporarily made the screen sort of centered -->
+  <!-- HACK: Temporarily made the screen sort of centered vertically -->
   <div class="h-screen flex items-center justify-center">
     <div class="flex flex-col w-full gap-5 max-w-5xl mx-auto items-center mb-15">
       <div class="flex flex-row w-full">
         <h1 class="text-5xl font-bold flex">Splash</h1>
-        <div class="text-lg flex items-center justify-end w-full"><h6>5/20</h6></div>
+        <div class="text-lg flex items-center justify-end w-full">
+          <h6>{{ current_index + 1 }}/{{ cards.length }}</h6>
+        </div>
       </div>
 
       <progress class="progress w-full" value="40" max="100"></progress>
 
       <div class="card bg-base-300 shadow-lg w-full">
         <div class="card-body">
-          <div class="h-50 flex justify-center items-center">
-            <p v-if="current_side == 'question'" class="text-3xl text-center w-full">
-              Sample Text of the front side of the card
-            </p>
-            <p v-else-if="current_side == 'answer'" class="text-lg text-center w-full">
-              Answer side of the flash card
-            </p>
+          <div v-if="cards.length">
+            <div class="h-50 flex justify-center items-center">
+              <p v-if="current_side == 'question'" class="text-3xl text-center w-full">
+                {{ sample_cards[current_index].question }}
+              </p>
+              <p v-else-if="current_side == 'answer'" class="text-lg text-center w-full">
+                {{ cards[current_index].answer }}
+              </p>
+            </div>
           </div>
 
           <div class="bg-base-600 flex flex-row w-full">
             <div class="w-1/3 flex justify-start">
-              <button class="btn font-light">< Previous</button>
+              <button class="btn font-light" @click="prevCard">< Previous</button>
             </div>
             <div class="w-1/3 flex justify-center">
               <button class="btn" @click="handleView">
@@ -52,7 +69,7 @@ const handleView = () => {
               </button>
             </div>
             <div class="w-1/3 flex justify-end">
-              <button class="btn">Next ></button>
+              <button class="btn" @click="nextCard">Next ></button>
             </div>
           </div>
         </div>
